89ac25bb153a9a5266f73225d636428c054fd5f6
Ryusei Fuji <cea0578@gmail.com>
2018-06-27 23:14:01 +0900

    Add ST function

--- statistics_s.py
+++ statistics_s.py
@@ -22,11 +22,45 @@ def SG(y, threshold, n1, n2, y_sigma):
         for y2 in y2_data:
             exp_sum += (1 - 2*y2 + 2*y2*x2 - x2**2) / (2 * y_sigma * y_sigma)
 
-        p_sum += x[1] * exp_sum
+        p_sum += x[1] * math.exp(exp_sum)
 
     ans = p_sum / p_x11
 
-    return (1/ans)
+    # (1,1)である
+    if 1/ans >= threshold:
+        return 1
+    # (1,1)でない
+    else:
+        return 0
+
+    
+
+def ST(y, threshold, n1, n2, y_sigma):
+    x_info = [((0,0), 0.6), ((0,1), 0.1), ((1,0),0.1), ((1,1), 0.2)]
+    y1_data = y[0:n1]
+    y2_data = y[n1:n2]
+    
+    #p(x00)の確率
+    p_x00 = x_info[0][1]
+    #p(x11)の確率
+    p_x11 = x_info[3][1]
+
+    p_sum = 0
+
+    for y1 in y1_data:
+        exp_sum += (1 - 2*y1) / (2 * y_sigma * y_sigma)
+
+    for y2 in y2_data:
+        exp_sum += (1 - 2*y2) / (2 * y_sigma * y_sigma)
+
+    ans = 1 + (p_x00 / p_x11) * math.exp(exp_sum)
+
+    # (1,1)である
+    if 1/ans >= threshold:
+        return 1
+    # (1,1)でない
+    else:
+        return 0
 
     
 
